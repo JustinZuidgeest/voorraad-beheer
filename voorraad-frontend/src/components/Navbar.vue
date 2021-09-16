48d0a14d86b9663a4a138d53cb8ed20801e909b1
Justin Zuidgeest <justinzuidgeest@gmail.com>
2021-09-17 00:19:05 +0200

    Replaced default boilerplate

---
+++ voorraad-frontend/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-app-bar app color="primary" dark>
+      
+    </v-app-bar>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
